1ebf4d49226e80cd87121bbb3055b07cc0632aa6
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2013-10-17 12:53:16 +0200

    initial commit with TSVReader and its test.

---
+++ src/test/java/de/konradhoeffner/commons/TSVReaderTest.java
@@ -0,0 +1,23 @@
+package de.konradhoeffner.commons;
+import static org.junit.Assert.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.junit.Test;
+
+public class TSVReaderTest
+{
+	@Test public void testTSVReader() throws IOException
+	{
+		int count=0;
+		try(TSVReader in = new TSVReader(this.getClass().getClassLoader().getResourceAsStream("codetocurrency.tsv")))
+		{
+		while(in.hasNextTokens())
+		{
+			count++;
+			String[] tokens = in.nextTokens();
+			assertTrue(tokens.length==2);
+		}
+		}
+		assertTrue(count>5);
+	}
+}
